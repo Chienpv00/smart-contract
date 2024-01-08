ca7a2eb6dc30a1de7abc84c01581adc022fbb7b2
chien.pham <chienpv00@gmail.com>
2024-01-08 21:36:00 +0700

    update structure

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -6,34 +6,73 @@ pragma solidity ^0.8.0;
 // Defines a contract named `HelloWorld`.
 // A contract is a collection of functions and data (its state). Once deployed, a contract resides at a specific address on the Ethereum blockchain. Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
 contract Transactions {
+    uint256 transactionCount;
 
-  uint256 transactionCount;
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 timestamp,
+        string fullName,
+        string phoneNumber,
+        string deliveryAddress,
+        uint256 paymentMethod,
+        string items
+    );
 
-    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
-  
-    struct TransferStruct {
+    struct OrderStruct {
         address sender;
         address receiver;
-        uint amount;
-        string message;
         uint256 timestamp;
-        string keyword;
+        string fullName;
+        string phoneNumber;
+        string deliveryAddress;
+        uint256 paymentMethod;
+        string items;
     }
 
-    TransferStruct[] transactions;
+    OrderStruct[] orders;
 
-    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+    function addToBlockchain(
+        address payable receiver,
+        string memory fullName,
+        string memory phoneNumber,
+        string memory deliveryAddress,
+        uint256 paymentMethod,
+        string memory items
+    ) public {
         transactionCount += 1;
-        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
-        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+        orders.push(
+            OrderStruct(
+                msg.sender,
+                receiver,
+                block.timestamp,
+                fullName,
+                phoneNumber,
+                deliveryAddress,
+                paymentMethod,
+                items
+            )
+        );
+
+        emit Transfer(
+            msg.sender,
+            receiver,
+            block.timestamp,
+            fullName,
+            phoneNumber,
+            deliveryAddress,
+            paymentMethod,
+            items
+        );
     }
 
-    function getAllTransactions() public view returns (TransferStruct[] memory) {
-        return transactions;
+    function getAllTransactions() public view returns (OrderStruct[] memory) {
+        return orders;
     }
 
     function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
+    
 }
